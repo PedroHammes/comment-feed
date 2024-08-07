e6f939428a0b1d3aa33c5f4c9caee7101310d130
Pedro Hammes <hammespedro4@gmail.com>
2024-08-07 11:27:54 -0300

    comment are created, but need be storaged

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
+import { useState } from 'react'
 import styles from './styles/app.module.css'
-import NewCommentForm from "./components/NewCommentForm/NewCommentForm";
-import Feed from './components/Feed/Feed';
+
 
 export default function App() {
+
+  const [ email, setEmail ] = useState('')
+  const [ comment, setComment ] = useState('')
+
+  const submitNewComment = (ev) => {
+    ev.preventDefault()
+    console.log('Your comment will be sent:')
+    console.log(`${email}\n${comment}`)
+    setEmail('')
+    setComment('')
+  }
+
   return (
     <div className={styles.app}>
+      
       <nav>
         <h1>Wellcome to our .Chat</h1>
       </nav>
       
-      <Feed />
+      <section className={styles.feed}>
+        <p>Comments will appear here</p>
+      </section>
       
-      <NewCommentForm />
+      <div className={styles.NewCommentForm}>
+            <input
+                type="text"
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
+            />
+
+            <textarea 
+              name=""
+              id=""
+              value={comment}
+              onChange={(ev) => setComment(ev.target.value)}
+            ></textarea>
+
+            <button onClick={submitNewComment}> ^ </button>
+      </div>
     </div>
   )
 }
--- src/components/Feed/Feed.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css'
-
-export default function Feed() {
-    return (
-        <section className={styles.feed}>
-            <p>Comments will appear here</p>
-        </section>
-    )
-}
