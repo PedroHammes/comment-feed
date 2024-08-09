df56598e10609bc2185fe9d97230b4da82f22867
Pedro Hammes <hammespedro4@gmail.com>
2024-08-09 11:26:24 -0300

    I configured the behavior and visibility of the form and the open form button

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,8 @@ export default function Form(props) {
 
     const [ email, setEmail ] = useState('')
     const [ comment, setComment ] = useState('')
-
+    const [ formVisibility, setFormVisibility ] = useState('none')
+    const [ openFormVisibility, setOpenFormVisibility ] = useState('flex')
 
     const formSubmit = (ev) => {
         ev.preventDefault()
@@ -13,13 +14,25 @@ export default function Form(props) {
         props.submitNewPost(email, comment)
         setEmail('')
         setComment('')
+        setFormVisibility('none')
+        setOpenFormVisibility('flex')
+    }
+
+    const openForm = (ev) => {
+        ev.preventDefault()
+        setOpenFormVisibility('none')
+        setFormVisibility('flex')
     }
 
     return (
-        <form 
-            className={styles.form}
-            onSubmit={formSubmit}
-        >
+        <div className={styles.formArea}>
+            <form 
+                className={styles.form}
+                onSubmit={formSubmit}
+                style={{
+                    display: `${formVisibility}`
+                }}
+            >
             <input
                 type="text"
                 placeholder='email@example.com'
@@ -28,12 +41,22 @@ export default function Form(props) {
             />
 
             <textarea 
-            placeholder='Your comment here'
-            value={comment}
-            onChange={(ev) => setComment(ev.target.value)}
-            ></textarea>
+                placeholder='Your comment here'
+                value={comment}
+                onChange={(ev) => setComment(ev.target.value)}
+            >
+            </textarea>
 
             <button> ^ </button>
         </form>
+
+        <button
+            className={styles.openForm}
+            style={{
+                display: `${openFormVisibility}`
+            }}
+            onClick={openForm}
+        > + </button>
+        </div>
     )
 }
